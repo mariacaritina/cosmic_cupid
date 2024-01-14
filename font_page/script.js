7ecafb8e99d0d7b262cf864591ae0ba928dabea0
Maria Ortiz <mortiz41@mkc02dp6fdmd6r.humad.com>
2024-01-14 16:45:42 -0500

    first

---
+++ font_page/script.js
@@ -0,0 +1,8 @@
+document.getElementById('exploreBtn').addEventListener('click', function() {
+    alert('Welcome to Cosmic Cupid! Start your cosmic journey.');
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! We will get back to you soon.');
+});
